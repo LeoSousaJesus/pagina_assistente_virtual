a0d31748217fba4341c338baef3ecae1381c53b7
LeoSousaJesus <leandrosousajesus@gmail.com>
2024-12-04 04:14:18 -0300

    Adicionando página de login interativa

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// JavaScript para interação ou funcionalidades adicionais (se necessário)
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página carregada com sucesso!");
+});
